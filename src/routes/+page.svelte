fa86c433843442a9a879d93912f3309627c390b8
deltea <thcheetah777@gmail.com>
2025-07-30 18:17:16 -0700

    speech stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,95 @@
 <script lang="ts">
-  const colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown", "gray", "lightgray", "black", "white"];
+  import { onMount } from "svelte";
 
-  
+  const colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown", "gray", "lightgray", "black", "white"];
+  const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+  let transcriptElement: HTMLElement;
+  let currentSpeech = "click to start";
+  let processedSpeech: string[];
+  let currentState = {
+    color: "red",
+    penDown: true,
+    position: { x: 0, y: 0 },
+    size: 5,
+  }
+
+  onMount(() => {
+    const recognition = new (window as any).webkitSpeechRecognition();
+    recognition.lang = "en-US";
+    recognition.continuous = true;
+
+    recognition.onresult = (e: any) => {
+      console.log("speech recognition result:", e);
+      currentSpeech = e.results[0][0].transcript.toLowerCase().trim();
+      processedSpeech = currentSpeech.split(" ");
+
+      if (processedSpeech.length > 0) {
+        processedSpeech[0] = convertToNumber(processedSpeech[0]);
+
+        // move pen
+        if (+processedSpeech[0] && processedSpeech.length >= 2) {
+          // move pen [0]px in direction of [1]
+          const distance = +processedSpeech[0];
+          const direction = processedSpeech[1];
+          if (direction === "up") {
+            currentState.position.y -= distance;
+          } else if (direction === "down") {
+            currentState.position.y += distance;
+          } else if (direction === "left") {
+            currentState.position.x -= distance;
+          } else if (direction === "right") {
+            currentState.position.x += distance;
+          } else {
+            console.warn("unrecognized direction:", direction);
+          }
+        } else if (processedSpeech[0] === "start") {
+          // start drawing
+          currentState.penDown = true;
+        } else if (processedSpeech[0] === "stop") {
+          // stop drawing
+          currentState.penDown = false;
+        } else if (processedSpeech[0] === "clear") {
+          // clear canvas
+        } else if (colors.includes(processedSpeech.join(""))) {
+          // change color to [0]
+          currentState.color = processedSpeech.join("");
+        } else {
+          console.warn("unrecognized command:", processedSpeech);
+        }
+      }
+
+      console.log("processed speech:", processedSpeech);
+      recognition.stop();
+    }
+    recognition.onstart = () => {
+      console.log("speech recognition started");
+      // transcriptElement.textContent = "listening...";
+    };
+    recognition.onerror = (e: any) => {
+      console.error("speech recognition error:", e);
+    };
+    recognition.onend = () => {
+      console.log("speech recognition ended");
+      recognition.start();
+    };
+
+    transcriptElement.addEventListener("click", () => {
+      recognition.start();
+    });
+  });
+
+  function convertToNumber(text: string): string {
+    if (numbers.includes(text)) {
+      return numbers.indexOf(text).toString();
+    } else {
+      return text;
+    }
+  }
 </script>
 
 <main class="h-full flex gap-8 p-8">
-  <div class="flex flex-col gap-2 h-full">
+  <div class="flex flex-col gap-6 h-full items-center">
     <!-- header -->
     <a href="/" class="text-[3.5rem] flex flex-col -space-y-8 font-bold">
       <h1>skribbl</h1>
@@ -24,13 +108,13 @@
       </div>
 
       <div class="overflow-y-auto grid grid-cols-2 gap-4 items-start">
-        {#each colors as color, i}
+        {#each colors as color}
           <div
             class="flex justify-center items-center border-3 rounded-xl aspect-square border-primary"
             style:background-color={color}
           >
-            {#if i === 0}
-              <iconify-icon icon="mingcute:check-2-fill" class="text-4xl text-bg"></iconify-icon>
+            {#if color === currentState.color}
+              <iconify-icon icon="mingcute:check-2-fill" class="text-4xl text-primary"></iconify-icon>
             {/if}
           </div>
         {/each}
@@ -44,12 +128,12 @@
   </div>
 
   <div class="flex flex-col gap-8 h-full grow">
-    <canvas class="border-3 border-primary bg-white rounded-xl p-8 w-full grow drop-shadow-xl">
-
-    </canvas>
+    <canvas class="border-3 border-primary bg-white rounded-xl p-8 w-full grow drop-shadow-xl"></canvas>
 
     <div class="flex justify-center items-center h-24 w-full">
-      <h2 class="font-bold text-primary text-5xl underline underline-offset-[12px]">pen up</h2>
+      <h2 bind:this={transcriptElement} class="font-bold text-primary text-5xl underline underline-offset-[12px]">
+        {currentSpeech}
+      </h2>
     </div>
   </div>
 </main>
