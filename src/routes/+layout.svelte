eebf3b96cc75878d968bb434fa9ffd5e4f4f7c6f
deltea <thcheetah777@gmail.com>
2025-07-30 21:46:07 -0700

    change title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,10 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+  <title>SKRIBBL: hardmode</title>
+</svelte:head>
+
 <div class="grid-background h-screen">
   {@render children()}
 </div>
